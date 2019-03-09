234e7d72f1f717fc097d30937d60477664726317
konojunya <junya03dance@gmail.com>
2019-03-09 17:39:24 +0900

    feat: listも一度しかAPIアクセスしないようにする

--- src/components/modules/Loading/index.tsx
+++ src/components/modules/Loading/index.tsx
@@ -1,3 +1,22 @@
 import * as React from "react";
+import styled from "styled-components";
 
-export const Loading = () => <p>loading...</p>;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(-50px);
+`;
+
+const Text = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+export const Loading = () => (
+  <Container>
+    <Text>NOW LOADING...</Text>
+  </Container>
+);
--- src/context/modules/Image/actions/FetchImages.ts
+++ src/context/modules/Image/actions/FetchImages.ts
@@ -1,6 +1,6 @@
 import { actionCreator } from "./actionCreator";
 import { ThunkAction } from "../../../type";
-import { ReduxAPIError } from "redux-api-struct";
+import { ReduxAPIError, ReduxAPIState } from "redux-api-struct";
 import { Params } from "../../../../api/FetchImages";
 
 import { FetchImagesResponse } from "../../../../api-types/response";
@@ -11,21 +11,23 @@ import { FetchImagesResponse } from "../../../../api-types/response";
 export const fetchImagesAction = actionCreator.async<void, FetchImagesResponse, ReduxAPIError>("FETCH_IMAGES");
 
 export function fetchImages(params: Partial<Params>): ThunkAction<void, any> {
-  return async (dispatch, _getState, { api }) => {
-    dispatch(fetchImagesAction.started());
+  return async (dispatch, getState, { api }) => {
+    if (!ReduxAPIState.isSuccess(getState().imageReducer.images.status)) {
+      dispatch(fetchImagesAction.started());
 
-    const res = await api.fetchImages(params);
-    if (res.status !== 200) {
-      dispatch(fetchImagesAction.failed({
-        error: {
-          statusCode: res.status
-        }
+      const res = await api.fetchImages(params);
+      if (res.status !== 200) {
+        dispatch(fetchImagesAction.failed({
+          error: {
+            statusCode: res.status
+          }
+        }))
+        return;
+      }
+
+      dispatch(fetchImagesAction.done({
+        result: res.data
       }))
-      return;
     }
-
-    dispatch(fetchImagesAction.done({
-      result: res.data
-    }))
   }
 }
